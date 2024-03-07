ec2116e7083b76d58a1047ae010c99bc3eac8c7c
podestal <luisrodriguezpodesta@gmail.com>
2024-03-07 09:09:03 -0500

    categories screen basic

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,24 @@
+import {CATEGORIES} from '../data/data'
+import { FlatList } from 'react-native'
+import CategoryGridTile from '../components/CategoryGridTile'
+
+const renderCategoryItem = (itemData) => {
+    return (
+        <CategoryGridTile 
+            title={itemData.item.title}
+            color={itemData.item.color}
+        />)
+}
+
+const CategoriesScreen = () => {
+  return (
+    <FlatList 
+        data={CATEGORIES}
+        keyExtractor={ item => item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+    />
+  )
+}
+
+export default CategoriesScreen
