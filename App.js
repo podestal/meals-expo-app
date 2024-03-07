ff15f3f265ced15b86b8bb36cd40b4e0ab17f353
podestal <luisrodriguezpodesta@gmail.com>
2024-03-07 13:54:05 -0500

    favoriteMelas context creted

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StyleSheet, Button, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavoritesContextProvider from './store/context/favoritesContext';
 import User from './components/User';
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
@@ -21,25 +22,27 @@ const BottomNavigator = () => {
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="MealsCategory">
-        <Stack.Screen 
-          name='MealsCategory' 
-          component={BottomNavigator} 
-          options={{
-            headerShown: false
-          }}
-        />
-        <Stack.Screen name='MealsOverview' component={MealsOverviewScreen} />
-        <Stack.Screen 
-          name='MealDetail' 
-          component={MealDetailScreen}
-          options={{
-            headerRight: () => <Button title='Tap me'/>
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <FavoritesContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="MealsCategory">
+          <Stack.Screen 
+            name='MealsCategory' 
+            component={BottomNavigator} 
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen name='MealsOverview' component={MealsOverviewScreen} />
+          <Stack.Screen 
+            name='MealDetail' 
+            component={MealDetailScreen}
+            options={{
+              headerRight: () => <Button title='Tap me'/>
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </FavoritesContextProvider>
   );
 }
 
